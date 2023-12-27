83a9c924d08b444bd8b9d678fcb1e52f8c632092
codingkinesis <ilanbarcohn@gmail.com>
2023-12-27 15:39:46 +0200

    add advance filter button (no function)

--- src/cmp/EmailFilter.jsx
+++ src/cmp/EmailFilter.jsx
@@ -1,8 +1,15 @@
 import { useEffect, useState } from "react"
-import imgSearch from '../assets/imgs/search.png';
+import imgSearch from '../assets/imgs/search.png'
+import imgFilter from '../assets/imgs/filter.png'
 
 export function EmailFilter({ filterBy , onSetFilter}) {
     const [filterByToEdit, setFilterByToEdit] = useState(filterBy)
+    const [advanceFilter, setAdvanceFilter] = useState(null)
+
+    function startAdvanceFilter() {
+        setAdvanceFilter(1)
+    }
+    
     
     useEffect(() => {
         onSetFilter(filterByToEdit)
@@ -17,7 +24,7 @@ export function EmailFilter({ filterBy , onSetFilter}) {
     return (
         <form className="email-filter">
             <label className="search-bar-container">
-                <img src={imgSearch} />
+                <img className="search" src={imgSearch} />
                 <input className="search-bar"
                     type="text" 
                     name="text" 
@@ -25,6 +32,11 @@ export function EmailFilter({ filterBy , onSetFilter}) {
                     placeholder={'Search mail'}
                     onChange={handleChange} 
                 />
+
+                <button> {/* add advance filters*/}
+                    <img className="filter" src={imgFilter} />
+                </button>
+                
             </label>
             <section className="dropdown-options">
                 <select className="dropdown" name="isRead" value={isRead} onChange={handleChange}>
